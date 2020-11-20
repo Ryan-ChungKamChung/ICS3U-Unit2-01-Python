6721dfbcb1d5323b2df706dceb38190e618d8092
Ubuntu <ubuntu@ip-172-31-48-242.ec2.internal>
2020-11-20 18:26:11 +0000

    added circumference.py

---
+++ circumference.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in November 2020
+# Program finding area and perimeter of a rectangle
+
+
+import math
+
+
+def main():
+    rad = 15
+    circ = math.pi*2*rad
+    area = math.pi*rad**2
+    print("The circumference of a circle with a radius of 15mm is:")
+    print(circ)
+    print("The area of a circle with a radius of 15mm is::")
+    print(area)
+
+
+if __name__ == "__main__":
+    main()
