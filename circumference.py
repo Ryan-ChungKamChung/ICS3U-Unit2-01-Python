c25df8ed907d56da748e8b7a86e4b8f3f19c0502
Ubuntu <ubuntu@ip-172-31-54-182.ec2.internal>
2020-11-23 16:32:17 +0000

    minor fixes

--- circumference.py
+++ circumference.py
@@ -2,21 +2,22 @@
 
 # Created by Ryan Chung Kam Chung
 # Created in November 2020
-# Program finding circumference of a circle
+# Program finding circumference and area of a circle
 
 
 import math
 
 
 def main():
-    superscript = str.maketrans("2", "²")
-
     rad = 15
     circ = math.pi*2*rad
     area = math.pi*rad**2
     print("The circumference of a circle with a radius of 15mm is:")
     print("{}mm".format(circ))
     print("The area of a circle with a radius of 15mm is::")
+
+    # https://codeigo.com/python/printing-subscript-and-superscript
+    superscript = str.maketrans("2", "²")
     print("{}mm2".translate(superscript).format(area))
 
 
